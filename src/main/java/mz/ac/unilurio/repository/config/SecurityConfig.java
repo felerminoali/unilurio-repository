1853f141e8d544dbec4641e478db0393de4e7145
felerminoali <felerminoali@gmail.com>
2020-09-23 14:44:02 +0200

    logout working

--- src/main/java/mz/ac/unilurio/repository/config/SecurityConfig.java
+++ src/main/java/mz/ac/unilurio/repository/config/SecurityConfig.java
@@ -2,12 +2,29 @@ package mz.ac.unilurio.repository.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .oauth2Login();
+    }
+
     @Bean
     public TokenStore tokenStore() {
         return new InMemoryTokenStore();
